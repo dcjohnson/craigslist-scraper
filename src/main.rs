5b943e4c61fa3a33981f518c607d46e2cdd0dc7f
Dylan Johnson <dylanatasmsa@gmail.com>
2016-03-09 21:53:23 -0700

    hyper works and it compiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate html5ever;
+extern crate hyper;
+
+use std::io::Read;
+use hyper::Client;
+use hyper::header::Connection;
+
 fn main() {
-    println!("Hello, world!");
+  let mut res = Client::new().get("https://provo.craigslist.org/search/roo?sort=date")
+    .header(Connection::close()).send().unwrap();
+  let mut body = String::new();
+  res.read_to_string(&mut body).unwrap();
+  println!("{}", body);
 }
