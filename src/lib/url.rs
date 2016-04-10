a75a0875898314830112a59de34d92699dcc780f
Dylan Johnson <dylanatasmsa@gmail.com>
2016-04-09 23:04:59 -0600

    url

---
+++ src/lib/url.rs
@@ -0,0 +1,24 @@
+pub struct Url {
+    location: String,
+    topic: String,
+    search_url: String
+}
+
+impl Url {
+    pub fn new(loc: String, topic: String, query: String) -> Self {
+        let search_url = format!("https://{location}.craigslist.org/search/{topic}?{query}", location=loc, topic=topic, query=query);
+        Url {
+            location: loc,
+            topic: topic,
+            search_url: search_url
+        }
+    }
+
+    pub fn make_post_url(&self, id: &String) -> String {
+        format!("https://{location}.craigslist.org/{topic}/{id}.html", location=self.location, topic=self.topic, id=id)
+    }
+
+    pub fn get_search_url(&self) -> &String {
+        &self.search_url
+    }
+}
